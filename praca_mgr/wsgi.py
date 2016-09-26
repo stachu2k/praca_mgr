f87ba48d2e0758281701d805ce7cbcb3e88bfdf1
Paweł Staś <p.stas@student.po.edu.pl>
2016-09-26 16:09:37 +0200

    no message

---
+++ praca_mgr/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for praca_mgr project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "praca_mgr.settings")
+
+application = get_wsgi_application()
